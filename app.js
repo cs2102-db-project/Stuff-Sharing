ff3e69de2a270c243835c9a2c4df5a5ec1c18b2e
DarylTew <tew.you.wen.daryl@gmail.com>
2019-03-28 23:26:58 +0800

    add a new page for add item

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var profileRouter = require('./routes/profile');
 var transactionRouter = require('./routes/transaction');
+var addItemRouter = require('./routes/addItem');
 
 var app = express();
 
@@ -23,10 +23,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/profile', profileRouter);
 app.use('/transaction', transactionRouter);
+app.use('/addItem', addItemRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
