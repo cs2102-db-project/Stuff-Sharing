2e979748907b3fac770a24d22333a058f6c4a037
DarylTew <tew.you.wen.daryl@gmail.com>
2019-03-28 21:00:33 +0800

    removed unnecessary buttons from login

--- public/javascripts/loginScript.js
+++ public/javascripts/loginScript.js
@@ -1,22 +1,3 @@
-// function check(event) {
-// 	// Get Values
-// 	var username    = document.getElementById('username').value;
-// 	var password = document.getElementById('password').value;
-	
-// 	// Simple Check
-// 	if(username.length == 0) {
-// 		alert("Invalid username");
-// 		event.preventDefault();
-// 		event.stopPropagation();
-// 		return false;
-// 	}
-// 	if(password.length == 0) {
-// 		alert("Invalid password");
-// 		event.preventDefault();
-// 		event.stopPropagation();
-// 		return false;
-// 	}
-// }
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
