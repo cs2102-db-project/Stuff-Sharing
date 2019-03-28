65cbdf82e462826ae9b6daffc6480b55aba125ee
DarylTew <tew.you.wen.daryl@gmail.com>
2019-03-28 21:40:08 +0800

    commented functionality for santisation for testing later

--- public/javascripts/loginScript.js
+++ public/javascripts/loginScript.js
@@ -9,3 +9,27 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
     container.classList.remove('right-panel-active');
 });
+
+/*
+var signUpName = document.getElementById('signUpName'),
+	signUpEmail = document.getElementById('signUpEmail'),
+	signUpPassword = document.getElementById('signUpPassword'),
+
+    signUpButton = document.getElementById('signUpButton').addEventListener('click', function(){
+      // Sanitize
+	  var clean = DOMPurify.sanitize(signUpName.value, {SAFE_FOR_TEMPLATES: true}) + DOMPurify.sanitize(signUpEmail.value, {SAFE_FOR_TEMPLATES: true}) 
+	  	+ DOMPurify.sanitize(signUpPassword.value, {SAFE_FOR_TEMPLATES: true});
+      result.innerHTML = clean;
+      console.log(clean);
+	});
+	
+var signInEmail = document.getElementById('signInEmail'),
+	signInPassword = document.getElementById('signInPassword'),
+
+	signInButton = document.getElementById('signInButton').addEventListener('click', function(){
+		// Sanitize
+		var clean = DOMPurify.sanitize(signInEmail.value, {SAFE_FOR_TEMPLATES: true}) + DOMPurify.sanitize(signInPassword.value, {SAFE_FOR_TEMPLATES: true});
+		result.innerHTML = clean;
+		console.log(clean);
+    });
+*/
