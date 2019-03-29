fbb91a45bd003d6c34aca4045bc7e25ece6257ee
adamwth <adamwth@hotmail.com>
2019-03-29 21:46:53 +0800

    remove sql.js and add init.sql

--- routes/index.js
+++ routes/index.js
@@ -16,17 +16,6 @@ const pool = new Pool({
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  pool.query(queries.migrate_database, (err, result) => {
-    if (err) {
-      return console.error('Error executing migrations', err.stack)
-    }
-  })
-  pool.query(queries.seed_database, (err, result) => {
-    if (err) {
-      return console.error('Error seeding database', err.stack)
-    }
-  })
-
   let val = indexController.random('Ayush');
   res.render('index', {title: 'Stuff Sharing', value: val});
 });
