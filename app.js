dcb3e4446c4b357dfa53601339f2b26bfe626798
adamwth <adamwth@users.noreply.github.com>
2019-03-29 21:55:21 +0800

    Remove sql.js and add init.sql (#6)

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
 var profileRouter = require('./routes/profile');
 var transactionRouter = require('./routes/transaction');
-var addItemRouter = require('./routes/addItem');
+var addItemRouter = require('./routes/additem');
 
 var app = express();
 
