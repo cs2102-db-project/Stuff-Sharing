79f368390e0ef605dec3e9ded83fdd0a8ca591ef
DarylTew <tew.you.wen.daryl@gmail.com>
2019-04-05 22:10:06 +0800

    set up sign in and sign up with database functionality too

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var sanitise = require('express-validator/check');
+
 var bodyParser = require('body-parser');
 
 // middleware for bodyParser
@@ -23,20 +25,26 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res) {
+
   var signInUsername = req.body.signInUsername;
   var signInPassword = req.body.signInPassword;
+  
   console.log(signInUsername);
   console.log(signInPassword);
-  pool.query('SELECT 1 FROM accounts WHERE accounts.username = $1::text AND accounts.password = $2::text', [signInUsername, signInPassword],
-    function (err, data) {
-      if (err) {
-        res.send(err);
-      }
-      if (data.rows.length == 0) {
-        res.send("Username or password is invalid");
-      } else {
-         res.redirect('/profile');
-      }
+
+  if (signInUsername.length == 0 || signInPassword.length == 0) {
+    return res.status(400).send("Username or Password is missing");
+  }
+  pool.query('SELECT 1 FROM accounts WHERE accounts.username = $1::text AND accounts.password = $2::text', [signInUsername, signInPassword], function (err, data) {
+    if (err) {
+      console.log('error');
+    }
+    if (data.rows.length == 0) {
+      console.log('hello');
+      res.status(400).send("Username or password is invalid");
+    } else {
+       res.redirect('/profile');
+    }
   });
 });
 
