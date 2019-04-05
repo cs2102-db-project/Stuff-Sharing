2d3b9380972d81c3e93c41c24a27a2938c58d58b
adamwth <adamwth@users.noreply.github.com>
2019-04-05 22:26:25 +0800

    Add Search feature (#11)
    * added migration of databases
    
    * seed database
    
    * fix bug
    
    * update package.json
    
    * fix sql date range
    
    * update cascade on migrations in sql.js
    
    * resolve merge conflict
    
    * remove sql.js and add init.sql
    
    * fix bugs in init.sql'
    
    * Implemented search POST request and receive response
    
    * Add search feature

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,38 @@ var express = require('express');
 var router = express.Router();
 var indexController = require('../controllers/indexController');
 
+// var queries = require('./sql');
+/* SQL Query */
+var sqlQuery = keyword => `SELECT * from Stuff where name = '${keyword}'`;
+
+const { Pool } = require('pg')
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'stuffsharing',
+  password: 'postgres',
+  port: 5432,
+})
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let val = indexController.random('Ayush');
   res.render('index', {title: 'Stuff Sharing', value: val});
+});
+
+/* Search feature */
+router.post('/search', function(req, res, next) {
+    let keyword = req.body.keyword;
+    let query = sqlQuery(keyword);
+    console.log(query);
+    pool.query(query, (err, result) => {
+        if (err) {
+          return console.error('Error executing query', err.stack)
+        }
+        res.render('index', {title: 'Stuff Sharing', value: indexController.random('Ayush'), test: '<p>hi</p>'});
+        return console.log(result.rows[0]);
+    });
 });
 
 module.exports = router;
