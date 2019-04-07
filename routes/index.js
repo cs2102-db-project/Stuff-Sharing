19f9d366549b4dfa09a080edbc8c020d482b236d
adamwth <adamwth@users.noreply.github.com>
2019-04-07 21:42:57 +0800

    Finish search (#13)
    * added migration of databases
    
    * seed database
    
    * fix bug
    
    * update package.json
    
    * fix sql date range
    
    * update cascade on migrations in sql.js
    
    * resolve merge conflict
    
    * remove sql.js and add init.sql
    
    * fix bugs in init.sql'
    
    * Implemented search POST request and receive response
    
    * Add search feature
    
    * search ui
    
    * finish search feature

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 var indexController = require('../controllers/indexController');
 
-// var queries = require('./sql');
 /* SQL Query */
-var sqlQuery = keyword => `SELECT * from Stuff where name = '${keyword}'`;
+var allQuery = 'SELECT * from Stuff';
+var searchQuery = 'SELECT * from Stuff where name=$1';
 
 const { Pool } = require('pg')
 const pool = new Pool({
@@ -18,21 +18,28 @@ const pool = new Pool({
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let val = indexController.random('Ayush');
-  res.render('index', {title: 'Stuff Sharing', value: val});
+    pool.query(allQuery, (err, result) => {
+        if (err) {
+          return console.error('Error executing query', err.stack)
+        }
+        res.render('index',
+            {title: 'Stuff Sharing',
+             value: result.rows});
+        return console.log(result.rows);
+    });
 });
 
 /* Search feature */
 router.post('/search', function(req, res, next) {
     let keyword = req.body.keyword;
-    let query = sqlQuery(keyword);
-    console.log(query);
-    pool.query(query, (err, result) => {
+    pool.query(searchQuery, [keyword], (err, result) => {
         if (err) {
           return console.error('Error executing query', err.stack)
         }
-        res.render('index', {title: 'Stuff Sharing', value: indexController.random('Ayush'), test: '<p>hi</p>'});
-        return console.log(result.rows[0]);
+        res.render('index',
+            {title: 'Stuff Sharing',
+             value: result.rows});
+        return console.log(result.rows);
     });
 });
 
