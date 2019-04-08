4a463bf5670a4fec37c54819115dde99a502285a
Ayush Chattoraj <ayush.chattoraj@gmail.com>
2019-04-08 20:53:57 +0800

    Fix merge conflict

--- routes/item.js
+++ routes/item.js
@@ -11,10 +11,41 @@ const pool = new Pool({
   port: 5432,
 });
 
+const sqlQuery = 'SELECT * from Stuff where stuffid=$1';
+const borrowQuery = '';
+
 router.get('/', function(req, res) {
     let id = req.query.stuffId;
-    let stuff = itemController.random(id);
-    res.render('item', {stuffFields: stuff, title: "Item"});
+    const user = req.app.get('current user');
+
+    pool.query(sqlQuery, [id], (err, result) => {
+        if (err) {
+            console.error("Error executing query", err.stack);
+        }
+        res.render('item', { title: 'Item', user: user, value: result.rows[0]});
+        console.log(result.rows[0]);
+    });
+});
+
+router.post('/borrow', function(req, res) {
+    const id = req.query.stuffId;
+    const num = req.body.number;
+    const email = req.body.email;
+    const user = req.app.get('current user');
+
+    pool.query(borrowQuery, [id, num, email], (err, result) => {
+        if (err) {
+            return console.error("Error executing query", err.stack);
+        }
+
+        if (!user) {
+            res.redirect('/login');
+        }
+
+        res.render('item',
+                {});
+        return console.log(result.rows);
+    });
 });
 
 module.exports = router;
