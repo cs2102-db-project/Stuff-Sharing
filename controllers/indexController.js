a3e13a3378671d79021acb0d09c23ebd33225dfb
adamwth <adamwth@hotmail.com>
2019-04-08 17:49:25 +0800

    refactor search

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,43 @@
+const { Pool } = require('pg')
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'stuffsharing',
+    password: 'postgres',
+    port: 5432,
+  })
+  
+/* SQL Query */
+var allQuery = 'SELECT * from Stuff';
+var searchQuery = 'SELECT * from Stuff where name=$1';
+
 exports.random = function(randomData) {
     return [1,2,3,4];
 };
+
+exports.renderSearch = function(res) {
+    pool.query(allQuery, (err, result) => {
+        if (err) {
+          return console.error('Error executing query', err.stack)
+        }
+        res.render('index',
+            {title: 'Stuff Sharing',
+             value: result.rows});
+        return console.log(result.rows);
+    });
+}
+
+exports.renderSearch = function(res, keyword) {
+    pool.query(searchQuery, [keyword], (err, result) => {
+        if (err) {
+          return console.error('Error executing query', err.stack)
+        }
+        res.render('index',
+            {title: 'Stuff Sharing',
+             value: result.rows});
+        return console.log(result.rows);
+    });
+}
 
 // Add more functions over here for using with the routes. Have a route for each individual action, and a corresponding
 // function. Need to read up more on POST commands though, because I guess that the extra data that would be coming in would be
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,6 @@ var express = require('express');
 var router = express.Router();
 var indexController = require('../controllers/indexController');
 
-/* SQL Query */
-var allQuery = 'SELECT * from Stuff';
-var searchQuery = 'SELECT * from Stuff where name=$1';
-
 const { Pool } = require('pg')
 const pool = new Pool({
   user: 'postgres',
@@ -18,29 +14,13 @@ const pool = new Pool({
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    pool.query(allQuery, (err, result) => {
-        if (err) {
-          return console.error('Error executing query', err.stack)
-        }
-        res.render('index',
-            {title: 'Stuff Sharing',
-             value: result.rows});
-        return console.log(result.rows);
-    });
+    indexController.renderSearch(res);
 });
 
 /* Search feature */
 router.post('/search', function(req, res, next) {
     let keyword = req.body.keyword;
-    pool.query(searchQuery, [keyword], (err, result) => {
-        if (err) {
-          return console.error('Error executing query', err.stack)
-        }
-        res.render('index',
-            {title: 'Stuff Sharing',
-             value: result.rows});
-        return console.log(result.rows);
-    });
+    indexController.renderSearch(res, keyword);
 });
 
 module.exports = router;
