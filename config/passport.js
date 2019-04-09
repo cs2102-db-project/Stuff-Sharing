2e772088b3dcd249224856256074da28e5fc8c1c
Daryl Tew <34118159+DarylTew@users.noreply.github.com>
2019-04-09 23:43:05 +0800

    Login passport (#23)
    * skeleton of passport.js added
    
    * required on all the signin and signup fields
    
    * before starting the passport experimentation
    
    * signup passport works
    
    * authentication success in multiple sessions
    
    * minor typos
    
    * refactor req.user.rows[0] into currentUser

---
+++ config/passport.js
@@ -0,0 +1,118 @@
+// load all the things we need
+var LocalStrategy = require('passport-local').Strategy;
+
+module.exports = function(passport) {
+
+    // =========================================================================
+    // passport session setup ==================================================
+    // =========================================================================
+    // required for persistent login sessions
+    // passport needs ability to serialize and unserialize users out of session
+
+    // used to serialize the user for the session
+    passport.serializeUser(function(user, done) {
+        // console.log("serialize user: " + JSON.stringify(user));
+        // console.log("serialize user.rows[0]: " + JSON.stringify(user.rows[0]));
+        done(null, user);
+    });
+
+    // used to deserialize the user
+    passport.deserializeUser(function(user, done) {
+        // console.log("deserialize user.username: " + user.username);
+        // console.log("deserialize user.password: " + user.password);
+        done(null, user);
+    });
+
+    // =========================================================================
+    // LOCAL LOGIN =============================================================
+    // =========================================================================
+    passport.use('local-login', new LocalStrategy({
+        // these fields are to validate the user - local strategy only uses username and password
+        // the 'strings' are the names of the fields from login.ejs where form is submitted
+        usernameField : 'signInUsername',
+        passwordField : 'signInPassword',
+        passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
+    },
+    function(req, username, password, done) {
+        if (username) username = username.toLowerCase(); // Use lower-case username to avoid case-sensitive username matching
+
+        console.log("username: " + username);
+        console.log("password: " + password);
+
+        var pool = req.app.get('pool');
+        pool.query('SELECT * FROM accounts WHERE accounts.username = $1 AND accounts.password = $2', [username, password], function (err, user) {
+            // if no user is found, return the message
+            if (user.rows[0] == undefined) {
+                console.log("no user is found");
+                return done(null, false, req.flash('loginMessage', 'No such user or wrong password found.'));
+            } else {
+                return done(null, user);
+            }
+        });
+    }));
+
+    // =========================================================================
+    // LOCAL SIGNUP ============================================================
+    // =========================================================================
+    // we are using named strategies since we have one for login and one for signup
+    // by default, if there was no name, it would just be called 'local'
+
+    passport.use('local-signup', new LocalStrategy({
+        // these fields are to validate the user - local strategy only uses username and password
+        // the 'strings' are the names of the fields from login.ejs where form is submitted
+        usernameField : 'signUpUsername',
+        passwordField : 'signUpPassword',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, username, password, done) {
+        if (username) username = username.toLowerCase(); // Use lower-case username to avoid case-sensitive username matching
+
+        console.log("username: " + username);
+        console.log("password: " + password);
+
+        var signUpName = req.body.signUpName;
+        var signUpPicture = req.body.signUpPicture;
+        var signUpAddress = req.body.signUpAddress;
+        console.log("name: " + signUpName);
+        console.log("picture: " + signUpPicture);
+        console.log("address: " + signUpAddress);
+
+        // load connection to database
+        var pool = req.app.get('pool');
+
+        // begin transaction, don't commit unless all makes it through
+        pool.query('BEGIN', function(err) {
+            if(err) {
+                console.log('begin error');
+                return done(err);
+            }
+            // find a user whose username is the same as the forms username
+            // we are checking to see if the user trying to login already exists
+            pool.query('INSERT INTO accounts (username, password) VALUES ($1, $2);', [username, password], function(err, user) {
+                // check to see if theres already a user with that username
+                if (!user) {
+                    console.log("This username is already taken")
+                    return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
+                } else {
+                    // insert into profiles
+                    pool.query('INSERT INTO profiles (username, name, picture, address) VALUES ($1, $2, $3, $4)', [username, signUpName, signUpPicture, signUpAddress], function(err) {
+                        if(err) {
+                            console.log('insertion into profile error');
+                            return done(err);
+                        } else {
+                            // end transaction after successful commit
+                            pool.query('COMMIT', function(err) {
+                                if(err) {
+                                    console.log('commit error');
+                                    return done(error);
+                                } else {
+                                    return done(null);
+                                }
+                            });
+                        }
+                    });
+                }
+            });    
+        });
+    }));
+};
--- routes/additem.js
+++ routes/additem.js
@@ -7,7 +7,8 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', isLoggedIn, function(req, res, next) {
+
   res.render('additem', { title: 'additem' });
 });
 
@@ -24,40 +25,62 @@ router.post('/', function(req, res) {
   console.log(itemType);
   console.log(itemDescription);
 
-  var currentUser = req.app.get('current user');
-  if (currentUser == null) {
-    res.redirect('/login');
-  }
-
   var pool = req.app.get('pool');
-  pool.query('BEGIN', function(err, data) {
+  var currentUser = req.user.rows[0];
+  pool.query('BEGIN', function(err) {
+    if (err) {
+      console.log('begin error');
+      return done(err);
+    }
     pool.query('SELECT MAX(stuffid) as stuffid FROM stuff;', function(err, data) {
+      if (err) {
+        console.log('select stuffid error');
+        return done(err);
+      }
       var itemId = data.rows[0].stuffid + 1;
       console.log("itemId = " + itemId);
       pool.query('INSERT INTO stuff (stuffid, picture, name, owner, price, description) VALUES ($1, $2, $3, $4, $5, $6);', 
-        [itemId, itemPicture, itemName, currentUser.username, itemPrice, itemDescription], function(err, data) {
-          if (err) console.log('stuff error');
-        
+        [itemId, itemPicture, itemName, currentUser.username, itemPrice, itemDescription], function(err) {
+        if (err) {
+          console.log('insertion into stuff error');
+          return done(err);
+        }
         if (itemType == "deliverables") {
           var itemDeliveryCost = req.body.itemDeliveryCost;
           console.log(itemDeliveryCost);
           if (itemDeliveryCost.length == 0) {
             return res.status(400).send("Please fill up delivery cost");
           } else {
-            pool.query('INSERT INTO deliverables (stuffid, deliverycost) VALUES ($1, $2);', [itemId, itemDeliveryCost], function(err, data) {
-              if (err) console.log('deliverables error');
+            pool.query('INSERT INTO deliverables (stuffid, deliverycost) VALUES ($1, $2);', [itemId, itemDeliveryCost], function(err) {
+              if (err) {
+                console.log('insertion into deliverables error');
+                return done(err);
+              }
               //disconnect after successful commit
-              pool.query('COMMIT', function(err, data) {
-                if(err) console.log('error1');
+              pool.query('COMMIT', function(err) {
+                if (err) {
+                  console.log('commit error');
+                  return done(err);
+                } else {
+                  res.redirect('/');
+                }  
               });
             });
           }
         } else if (itemType == "services") {
           pool.query('INSERT INTO services (stuffid) VALUES ($1);', [itemId], function(err, data) {
-            if (err) console.log('services error');
+            if (err) {
+              console.log('insertion into services error');
+              return done(err);
+            }
             //disconnect after successful commit
-            pool.query('COMMIT', function(err, data) {
-              if(err) console.log('error2');
+            pool.query('COMMIT', function(err) {
+              if (err) {
+                console.log('commit error');
+                return done(err);
+              } else {
+                res.redirect('/');
+              }              
             });
           });
         } else if (itemType == "pickups") {
@@ -66,28 +89,53 @@ router.post('/', function(req, res) {
           if (itemPickupAddress.length == 0) {
             return res.status(400).send("Please fill up pickup address");
           } else {
-            pool.query('INSERT INTO pickups (stuffid, pickupaddress) VALUES ($1, $2);', [itemId, itemPickupAddress], function(err, data) {
-              if (err) console.log('pickups error');
+            pool.query('INSERT INTO pickups (stuffid, pickupaddress) VALUES ($1, $2);', [itemId, itemPickupAddress], function(err) {
+              if (err) {
+                console.log('insertion into pickups error');
+                return done(err);
+              }              
               //disconnect after successful commit
-              pool.query('COMMIT', function(err, data) {
-                if(err) console.log('error3');
+              pool.query('COMMIT', function(err) {
+                if (err) {
+                  console.log('commit error');
+                  return done(err);
+                } else {
+                  res.redirect('/');
+                }          
               });
             });
           }
         } else {
-          pool.query('INSERT INTO intangibles (stuffid) VALUES ($1);', [itemId], function(err, data) {
-            if (err) console.log('intangibles error');
+          pool.query('INSERT INTO intangibles (stuffid) VALUES ($1);', [itemId], function(err) {
+            if (err) {
+              console.log('insertion into intangibles error');
+              return done(err);
+            }            
             //disconnect after successful commit
-            pool.query('COMMIT', function(err, data) {
-              if(err) console.log('error4');
+            pool.query('COMMIT', function(err) {
+              if (err) {
+                console.log('commit error');
+                return done(err);
+              } else {
+                res.redirect('/');
+              }         
             });
           });
         }
-
       });
     });
-    res.redirect('/');
   });
 });
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+  // if user is authenticated in the session, carry on 
+  if (req.isAuthenticated())
+      return next();
+
+  // if they aren't redirect them to the home page
+  res.redirect('/login');
+}
 
 module.exports = router;
