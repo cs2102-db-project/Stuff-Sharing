6f86d4df74392237a61933a1261c6d8ff763b6ba
adamwth <adamwth@users.noreply.github.com>
2019-04-09 22:17:27 +0800

    Refactor Search and add support to display category under Browse (#16)
    * added migration of databases
    
    * seed database
    
    * fix bug
    
    * update package.json
    
    * fix sql date range
    
    * update cascade on migrations in sql.js
    
    * resolve merge conflict
    
    * remove sql.js and add init.sql
    
    * fix bugs in init.sql'
    
    * Implemented search POST request and receive response
    
    * Add search feature
    
    * search ui
    
    * finish search feature
    
    * refactor search
    
    * Refactor search
    
    * Add browse category support
    
    * Resolve merge conflict
    
    * Update Browse categories across all pages
    
    * Remove creation of pool in index.js and indexController.js
    
    * remove pool again sry

--- routes/index.js
+++ routes/index.js
@@ -2,45 +2,19 @@ var express = require('express');
 var router = express.Router();
 var indexController = require('../controllers/indexController');
 
-/* SQL Query */
-var allQuery = 'SELECT * from Stuff';
-var searchQuery = 'SELECT * from Stuff where name=$1';
-
-const { Pool } = require('pg')
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'stuffsharing',
-  password: 'postgres',
-  port: 5432,
-})
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    pool.query(allQuery, (err, result) => {
-        if (err) {
-          return console.error('Error executing query', err.stack)
-        }
-        res.render('index',
-            {title: 'Stuff Sharing',
-             value: result.rows});
-        return console.log(result.rows);
-    });
+    indexController.renderAll(req, res);
 });
 
-/* Search feature */
-router.post('/search', function(req, res, next) {
-    let keyword = req.body.keyword;
-    pool.query(searchQuery, [keyword], (err, result) => {
-        if (err) {
-          return console.error('Error executing query', err.stack)
-        }
-        res.render('index',
-            {title: 'Stuff Sharing',
-             value: result.rows});
-        return console.log(result.rows);
-    });
+/* Search */
+router.get('/search', function(req, res, next) {
+    indexController.renderSearch(req, res);
 });
+
+/* Browse category */
+router.get('/category', function(req, res, next) {
+    indexController.renderCategory(req, res);
+})
 
 module.exports = router;
