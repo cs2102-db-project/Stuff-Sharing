888dba42ef90f576c600890183ce81fa6316f2e3
adamwth <adamwth@hotmail.com>
2019-04-11 00:05:59 +0800

    Finish adding end transaction

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -1,5 +1,6 @@
 /* SQL Query */
 var insertReviewQuery = 'INSERT INTO REVIEWS VALUES((SELECT max(reviewid) from REVIEWS) + 1, $1, $2, $3)';
+var endTransactionQuery = "UPDATE TRANSACTIONS SET STATUS = 'FINISHED' WHERE transid=$1"
 
 exports.addReview = function(req, res) {
     console.log("Adding review...");
@@ -15,4 +16,18 @@ exports.addReview = function(req, res) {
         return console.log(result.rows);
     });
     res.redirect('/profile_transactions');
+}
+
+exports.endTransaction = function(req, res) {
+  console.log("Ending transaction...");
+  console.log(req.body);
+  var pool = req.app.get('pool');
+  var transid = req.body.transid;
+  pool.query(endTransactionQuery, [transid], (err, result) => {
+      if (err) {
+        return console.error('Error executing query', err.stack)
+      }
+      return console.log(result.rows);
+  });
+  res.redirect('/profile_transactions');
 }
