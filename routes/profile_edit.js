ca024a6c3c76308b74dc1a1d6ad65b1ad8c128e4
chelchia <chelseachiajh98@gmail.com>
2019-04-11 15:13:21 +0800

    make profile pages compatible with login, fix bugs

--- routes/profile_edit.js
+++ routes/profile_edit.js
@@ -3,13 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var currentUser = req.app.get('current user');
-  if (currentUser == null) {
+  if (req.user == null) {
     res.redirect('/login');
   } else {
+    var currentUser = req.user.rows[0];
     res.render('profile_edit', {user: currentUser});
   }
 });
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+  // if user is authenticated in the session, carry on 
+  if (req.isAuthenticated())
+      return next();
+
+  // if they aren't redirect them to the login page
+  res.redirect('/login');
+}
 
 router.post('/', function(req, res) {
   //get inputs
@@ -18,7 +29,7 @@ router.post('/', function(req, res) {
   var address = req.body.address;
 
   //queries
-  var currentUser = req.app.get('current user');
+  var currentUser = req.user.rows[0];
   var pool = req.app.get('pool');
   var editAllQuery = 'UPDATE profiles SET name = $1::text, password = $2::text, address = $3::text WHERE username = $4::text';
   var editKeepPwdQuery = 'UPDATE profiles SET name = $1::text, address = $2::text WHERE username = $3::text';
