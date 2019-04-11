0156476e27c5763e254799023a87a58657d07a76
chelchia <34341767+chelchia@users.noreply.github.com>
2019-04-12 02:18:55 +0800

    Profile stats (#31)
    * Added profile stats page. Added complex query to find most popular item on the user's listing
    
    * fixed user bug in profile transaction page
    
    * fixed editing password bug

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,8 +1,30 @@
 /* SQL Query */
 var getUserProfileQuery = 'SELECT * FROM profiles WHERE username = $1';
 var getUserStuffQuery = 'SELECT * FROM stuff WHERE stuff.owner = $1';
-var editProfileAllQuery = 'UPDATE profiles SET name = $1::text, password = $2::text, address = $3::text WHERE username = $4::text';
-var editProfileKeepPwdQuery = 'UPDATE profiles SET name = $1::text, address = $2::text WHERE username = $3::text';
+var editPasswordQuery = 'UPDATE accounts SET password = $1::text WHERE username = $2::text';
+var editProfileQuery = 'UPDATE profiles SET name = $1::text, address = $2::text WHERE username = $3::text';
+var getItemNumLoansQuery = '\
+with itemCount as (\
+  SELECT transactions.stuffId as stuffId, count(*) as numLoans\
+  FROM transactions\
+  WHERE transactions.loanee = $1\
+  GROUP BY stuffId\
+  )\
+SELECT *\
+FROM stuff natural join itemCount\
+WHERE stuff.stuffId = itemCount.stuffId and itemCount.numLoans = (SELECT max(numLoans) FROM itemCount)';
+var getItemNumLoansQuery2 = '\
+with maxItem as (\
+  SELECT stuffId as stuffId, count(*) as numLoans\
+  FROM transactions\
+  WHERE transactions.loaner = $1\
+  GROUP BY stuffId\
+  ORDER BY numLoans desc\
+  LIMIT 1\
+  )\
+SELECT *\
+FROM stuff natural join maxItem'
+;
 
 
 /* Gets current user's profile (which includes username, picture name, address) */
@@ -44,6 +66,7 @@ exports.displayProfile = function(req, res) {
   });
 }
 
+/* Display form to edit profile */
 exports.displayEditProfileForm = function(req, res) {
   var currentUser = req.user.rows[0];
   // var profile = getCurrentUserProfile(req, res);
@@ -61,47 +84,56 @@ exports.displayEditProfileForm = function(req, res) {
 
 }
 
+/* Edits DB profile table */
 exports.editProfile = function(req, res) {
   //get inputs
   var name = req.body.name;
   var password = req.body.password;
   var address = req.body.address;
 
-  //queries
   var currentUser = req.user.rows[0];
   var pool = req.app.get('pool');
 
   if (password.length != 0) {
-    pool.query(editProfileAllQuery, [name, password, address, currentUser.username], (err, result) => {
+    pool.query(editPasswordQuery, [password, currentUser.username], (err, result) => {
         if (err) {
           return console.error('Error executing query', err.stack)
         }
-        pool.query(getUserProfileQuery, [currentUser.username], (err, result) => {
-            if (err) {
-              return console.error('Error executing query', err.stack)
-            } else {
-              console.log(result.rows[0]);
-              req.app.set('current user', result.rows[0]);
-              res.redirect('/profile');
-            }
-        });
-    });
-  } else {
-    pool.query(editProfileKeepPwdQuery, [name, address, currentUser.username], (err, result) => {
-        if (err) {
-          return console.error('Error executing query', err.stack)
-        }
-        pool.query(getUserProfileQuery, [currentUser.username], (err, result) => {
-            if (err) {
-              return console.error('Error executing query', err.stack)
-            } else {
-              console.log(result.rows[0]);
-              req.app.set('current user', result.rows[0]);
-              res.redirect('/profile');
-            }
-        });
     });
   }
+  pool.query(editProfileQuery, [name, address, currentUser.username], (err, result) => {
+    if (err) {
+      return console.error('Error executing query', err.stack)
+    }
+    pool.query(getUserProfileQuery, [currentUser.username], (err, result) => {
+        if (err) {
+          return console.error('Error executing query', err.stack)
+        } else {
+          console.log(result.rows[0]);
+          req.app.set('current user', result.rows[0]);
+          res.redirect('/profile');
+        }
+    });
+  });
+}
+
+/* Display profile stats */
+exports.displayProfileStats = function(req, res) {
+  var currentUser = req.user.rows[0];
+  var pool = req.app.get('pool');
+
+  pool.query(getUserProfileQuery, [currentUser.username], (err, result) => {
+    var profile = result.rows[0];
+
+    pool.query(getItemNumLoansQuery2, [currentUser.username], (err, result) => {
+      if (err) {
+        return console.error('Error executing query', err.stack)
+      } else {
+        var mostPopularItem = result.rows[0];
+        res.render('profile_stats', { user: profile, mostPopularItem: mostPopularItem });
+      }
+    });
+  });
 }
 
 
