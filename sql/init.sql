d1e100a63440b3b93911191e44b24b700ecc9dc3
Ayush Chattoraj <ayush.chattoraj@gmail.com>
2019-04-12 21:21:28 +0800

    Update init

--- sql/init.sql
+++ sql/init.sql
@@ -96,33 +96,83 @@ CREATE TABLE IF NOT EXISTS ads(
 
 INSERT INTO accounts VALUES
     ('johndoe', 'johndoe'),
-    ('janedoe', 'janedoe');
+    ('janedoe', 'janedoe'),
+    ('ayush', 'password'),
+    ('chelsea', 'password'),
+    ('daryl', 'password'),
+    ('adam', 'password');
 INSERT INTO admins VALUES
-    ('johndoe');
+    ('johndoe'),
+    ('ayush');
 INSERT INTO nonadmins VALUES
-    ('janedoe');
+    ('janedoe'),
+    ('daryl'),
+    ('adam'),
+    ('chelsea');
 INSERT INTO profiles VALUES
     ('johndoe', 'John Doe', 'picture?', '10 john road'),
-    ('janedoe', 'Jane Doe', 'picture?', '10 jane road');
+    ('janedoe', 'Jane Doe', 'picture?', '10 jane road'),
+    ('ayush', 'Ayush', 'picture?', '10 Ayush road'),
+    ('daryl', 'Daryl', 'picture?', '10 Daryl road'),
+    ('chelsea', 'Chelsea', 'picture?', '10 Chelsea road'),
+    ('adam', 'Adam', 'picture?', '10 Adam road');
 INSERT INTO stuff VALUES
-    (1, 'picture?', 'book', 'johndoe', 1.50, 'its a book'),
-    (2, 'picture?', 'car', 'johndoe', 2.50, 'its a car'),
-    (3, 'picture?', 'car wash', 'janedoe', 3.50, 'its a car wash'),
-    (4, 'picture?', 'math notes', 'janedoe', 4.50, 'its math notes');
+    (1, 'picture?', 'Math book', 'johndoe', 1.50, 'its a math book'),
+    (2, 'picture?', 'Car', 'johndoe', 2.50, 'its a car'),
+    (3, 'picture?', 'Car wash', 'janedoe', 3.50, 'its a car wash'),
+    (4, 'picture?', 'Math notes', 'janedoe', 4.50, 'its math notes'),
+    (5, 'picture?', 'Shoes', 'ayush', 1.50, 'Size 8 Shoes'),
+    (6, 'picture?', 'Chair', 'ayush', 2.50, 'Really comfortable'),
+    (7, 'picture?', 'Plumbing', 'ayush', 3.50, 'I will fix your plumbing issues'),
+    (8, 'picture?', 'Tutor', 'daryl', 1.50, 'I can tutor A-level Math'),
+    (9, 'picture?', 'Guitar', 'daryl', 2.50, 'Electric guitar to jam'),
+    (10, 'picture?', 'Truck', 'daryl', 3.50, 'Pick-up truck'),
+    (11, 'picture?', 'Sofa', 'chelsea', 3.50, 'Comfy sofa'),
+    (12, 'picture?', 'TV', 'chelsea', 3.50, 'Widescreen plasma'),
+    (13, 'picture?', 'Bottle', 'chelsea', 3.50, 'To drink water'),
+    (14, 'picture?', 'Table', 'chelsea', 3.50, 'Dinner table, looks great'),
+    (15, 'picture?', 'eBook', 'chelsea', 3.50, 'I will email it to you'),
+    (16, 'picture?', 'Vacation Photos', 'adam', 3.50, 'Really pretty photos! Great wallpapers'),
+    (17, 'picture?', 'Web Developer', 'adam', 3.50, 'Ill make ExpressJS websites for you'),
+    (18, 'picture?', 'Wedding photography', 'adam', 3.50, 'Ill shoot you'),
+    (19, 'picture?', 'Bag', 'ayush', 3.50, 'Louis Vuitton'),
+    (20, 'picture?', 'Phone', 'ayush', 3.50, 'Samsung S9');
+
 INSERT INTO deliverables VALUES
-    (1, 1.00);
+    (1, 1.00),
+    (5, 0.00),
+    (6, 5.00),
+    (9, 2.00),
+    (13, 0.00),
+    (19, 0.00),
+    (20, 0.00);
 INSERT INTO pickups VALUES
-    (2, '10 john road');
+    (2, '10 john road'),
+    (10, 'anywhere'),
+    (11, 'my home'),
+    (12, 'my house'),
+    (14, '11 john road');
 INSERT INTO intangibles VALUES
-    (4);
+    (4),
+    (15),
+    (16);
 INSERT INTO services VALUES
-    (3);
+    (3),
+    (7),
+    (8),
+    (17),
+    (18);
 INSERT INTO transactions VALUES
-    (1, 'janedoe', 1, '83365620', 'janedoe@joe.com', 'FINISHED', 10.00, '2019-01-01', '2019-01-20', 12.42);
+    (1, 'janedoe', 1, '83365620', 'janedoe@joe.com', 'FINISHED', 10.00, '2019-01-01', '2019-01-20', 12.42),
+    (2, 'ayush', 2, '81823456', 'ayush@ayush.com', 'ONGOING', 10.00, '2019-05-20', '2019-05-21', 15.00),
+    (3, 'daryl', 5, '12324212', 'asdad@gds.com', 'PENDING', 10.00, '2019-06-13', '2019-06-16', 20.00);
 INSERT INTO reviews VALUES
     (1, 1, 5, 'good');
 INSERT INTO ads VALUES
-    (2, 'johndoe');
+    (2, 'johndoe'),
+    (3, 'janedoe'),
+    (6, 'ayush'),
+    (14, 'chelsea');
 
 -- Prevent insertion if there are more than X overdue items
 CREATE OR REPLACE FUNCTION check_overdue()
